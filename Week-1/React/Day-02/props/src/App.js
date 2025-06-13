0afedfa89855fc5db56bb47463d48f5fbf3843b9
harshal yadav <harshal.yadav@mindbowser.com>
2025-06-13 10:24:00 +0530

    day two

---
+++ Week-1/React/Day-02/props/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ChildOne from './components/ChildOne';
+function App() {
+  return (
+    <div className="App">
+      <ChildOne/>
+    </div>
+  );
+}
+
+export default App;
