5e3609a74ad6f82574ffef841f5200a367807080
harshal yadav <harshal.yadav@mindbowser.com>
2025-07-17 23:00:51 +0530

    validation

--- Week-2/Nodejs/Week-04/util/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('mydb1223', 'postgres', 'newpassword123', {
-  host: 'localhost',
-  dialect:  'postgres',
-});
-module.exports = sequelize;
-
-// console.log("sequelize",sequelize);
