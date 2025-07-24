15f916b70c0bafb698354484baf935efaea34778
harshal yadav <harshal.yadav@mindbowser.com>
2025-07-24 21:27:14 +0530

    ts api completed

--- Week-07/Nodejs/index.js
+++ Week-07/Nodejs/index.js
@@ -1,24 +1,65 @@
-const express= require('express');
-const feedRoutes= require('./routes/feed');
-const app = express();
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const multer  = require('multer')
+const multer = require('multer');
 
-let _db;
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
-
+const app = express();
 const PORT=8080;
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
 const MONGODB_URI ='mongodb+srv://harshalyadav:r5HFz3Q8JM336wek@learning.5gn8wxw.mongodb.net/feed?retryWrites=true&w=majority&appName=Learning';
-app.use(express.json());
-app.use((req,res, next)=>{
- res.setHeader('Access-Control-Allow-Origin','*');
- res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE, PUT');
- res.setHeader('Access-Control-Allow-Header','Content-Type, Authorization');
-})
-
-app.use('/feed',feedRoutes);
-
-
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
